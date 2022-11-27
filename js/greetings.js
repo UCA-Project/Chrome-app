7692058549efa2ddd51fa82ed7371a1da5b4e6b8
NormalHuman <72422431+NormalHuman-Anything@users.noreply.github.com>
2022-11-27 18:11:38 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -1,3 +1,12 @@
 let loginForm = document.querySelector("#login-form")
-let loginInput = loginFomrm.querySelector("#login-input")
+let loginInput = loginForm.querySelector("#login-input")
 let loginBtn = loginForm.querySelector("#login-button")
+let greeting = document.querySelector("#greeting")
+
+loginInput.value
+
+function onLoginSubmit(){
+    let name = loginInput.value
+    console.log(name)
+}
+loginBtn.addEventListener("click", onLoginSubmit)

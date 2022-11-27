2cd48adf1374cec0581a402a543fb8da85ef8f3e
NormalHuman <72422431+NormalHuman-Anything@users.noreply.github.com>
2022-11-27 18:46:08 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -2,8 +2,23 @@ let loginForm = document.querySelector("#login-form")
 let loginInput = loginForm.querySelector("#login-input")
 let loginBtn = loginForm.querySelector("#login-button")
 let greeting = document.querySelector("#greeting")
+let userName = localStorage.getItem("userName")
 
-
+if(userName != null) {
+    getting.innerText = `안녕하세요, ${userName}님!`
+    loginForm.classList.add("hidden")
+} else {
+    function onLoginSubmit(e) {
+        let name = loginInput.value
+        console.log(name)
+        e.preventDefault()
+        greeting.innerText = `안녕하세요, ${name}님!`
+        loginForm.classList.add("hidden")
+        localStorage.setItem("userName", name)
+    }
+    loginForm.addEventListener("submit", onLoginSubmit)
+}
+/*
 function onLoginSubmit(e){
     let name = loginInput.value
     console.log(name)
@@ -14,3 +29,5 @@ function onLoginSubmit(e){
 }
 loginForm.classList.remove("hidden")
 loginForm.addEventListener("submit", onLoginSubmit)
+*/
+

825155bce6c7a24319d10fdbce52190be3ee60a7
devon_p <89315745+parkkh07@users.noreply.github.com>
2022-12-10 16:20:12 +0900

    .

--- js/greeting.js
+++ js/greeting.js
@@ -2,10 +2,17 @@ let loginForm = document.querySelector("#login-form");
 let loginInput = document.querySelector("#login-input");
 let loginBtn = document.querySelector("#login");
 let greeting = document.querySelector("#greeting");
+let savedName = localStorage.getItem("userName");
 
-function onLoginSubmit(event) {
-  let userName = loginInput.value;
-  alert(`Hello ${userName}`);
-  event.preventDefault();
+if (savedName == null) {
+  function onLoginSubmit(event) {
+    event.preventDefault();
+    let userName = loginInput.value;
+    localStorage.setItem("userName", userName);
+
+    greeting.innerText = `HI ${userName}`;
+  }
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  greeting.innerText = `HI ${userName}`;
 }
-loginForm.addEventListener("submit", onLoginSubmit);
